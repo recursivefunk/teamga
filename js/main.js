94c1c11557b54d078ea71109af00042c884f4dd6
johnny-ray <johnny@johnnyray.me>
2014-03-08 18:34:47 -0500

    stuff

--- js/main.js
+++ js/main.js
@@ -27,6 +27,21 @@
     .controller('MainCtrl', function($scope, dataService){
 
       $scope.currentUser = dataService.currentUser;
+      $scope.amountEntered = 0;
+      $scope.options = [
+        'banannas',
+        'apples',
+        'oranges'
+      ];
+      $scope.$watch('amountEntered', function(newAmount, oldAmount){
+        if ( newAmount >= 10 ) {
+          $scope.optionAfforded = $scope.options[ 0 ];
+        } else if ( newAmount <= 9 && newAmount >= 5 ) {
+          $scope.optionAfforded = $scope.options[ 1 ];
+        } else {
+          $scope.optionAfforded =  'nada';
+        }
+      });
 
     })
 

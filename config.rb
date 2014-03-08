72155fdee291f09f4a98583e20cc2d1c9f006ffa
johnny-ray <johnny@johnnyray.me>
2014-03-08 15:25:48 -0500

    first commit

---
+++ config.rb
@@ -0,0 +1,13 @@
+# Require any additional compass plugins here.
+require 'susy'
+# require 'modular-scale'
+
+# Set this to the root of your project when deployed:
+http_path = '/'
+
+css_dir = 'css'
+sass_dir = 'sass'
+images_dir = 'img'
+fonts_dir = 'fonts'
+javascripts_dir = 'js'
+relative_assets = true
